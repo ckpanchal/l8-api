9a7a32f2a9a649e5bc36584ac85dc3c889f81e22
Chintan Panchal <chintan@maxput.com>
2021-02-16 23:47:54 +0530

    Add contact_no field and setup roles

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('contact_no');
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/seeders/RoleAndAdminSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use App\Models\User;
+use App\Models\Role;
+use Illuminate\Support\Facades\Hash;
+
+class RoleAndAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $roles = 'Admin,User';
+        $rolesArray = explode(',', $roles);
+        foreach ($rolesArray as $role) {
+        	$roleObj = Role::Create([
+                'guard_name' 	=> 'api',
+                'name' 			=> trim($role)
+            ]);
+            if($roleObj && ($role == 'Admin')) {
+            	$this->createAdmin($roleObj);
+            }
+        }
+    }
+
+    private function createAdmin($role)
+    {
+        $user = User::create([
+            'email' 		=> 'admin@example.com',
+            'password' 		=> Hash::make('secret'),
+            'name' 			=> 'Super Admin',
+            'contact_no' 	=> '1234567890',
+            'created_at' 	=> Carbon::now(),
+        ]);
+        $user->assignRole($role);
+    }
+}
