99dd10d0b3df6419abc03b8adc69c653fe6fbe3c
Chintan Panchal <chintan@maxput.com>
2021-02-17 22:43:31 +0530

    Implement swagger api docs

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,6 +12,61 @@ use JWTAuth;
 
 class AuthController extends Controller
 {
+    /**
+     * @OA\Post(
+     * path="/api/register",
+     * summary="Register a new user",
+     * security={{"bearer_token":{}}},
+     * description="Register a new user",
+     * operationId="register",
+     * tags={"Auth"},
+     * @OA\Parameter(
+     *    description="User Name",
+     *    in="query",
+     *    name="name",
+     *    required=true,
+     *    @OA\Schema(
+     *       type="string"
+     *    )
+     * ),
+     * @OA\Parameter(
+     *    description="User Username (Unique)",
+     *    in="query",
+     *    name="username",
+     *    required=true,
+     *    @OA\Schema(
+     *       type="string",
+     *    )
+     * ),
+     * @OA\Parameter(
+     *    description="User Email (Unique)",
+     *    in="query",
+     *    name="email",
+     *    required=true,
+     *    @OA\Schema(
+     *       type="string"
+     *    )
+     * ),
+     * @OA\Parameter(
+     *    description="User Contact Number",
+     *    in="query",
+     *    name="contact_no",
+     *    required=true,
+     *    example="9876543210",
+     *    @OA\Schema(
+     *       type="integer",
+     *       format="int64"
+     *    )
+     * ),
+     * @OA\Response(
+     *    response=201,
+     *    description="User successfully registered",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="User successfully registered")
+     *        )
+     *     )
+     * )
+     */
     public function register(RegisterRequest $request) 
     {
         $user = User::create(array_merge(
@@ -28,26 +83,123 @@ class AuthController extends Controller
         ], 201);
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/login",
+     * summary="Login User",
+     * security={{"bearer_token":{}}},
+     * description="Login User",
+     * operationId="login",
+     * tags={"Auth"},
+     * @OA\Parameter(
+     *    description="Username",
+     *    in="query",
+     *    name="username",
+     *    required=true,
+     *    @OA\Schema(
+     *       type="string",
+     *    )
+     * ),
+     * @OA\Parameter(
+     *    description="Password",
+     *    in="query",
+     *    name="password",
+     *    required=true,
+     *    @OA\Schema(
+     *       type="string",
+     *    )
+     * ),
+     * @OA\Response(
+     *    response=401,
+     *    description="Unauthorized",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="Unauthorized")
+     *        )
+     *     )
+     * ),
+     * @OA\Response(
+     *    response=200,
+     *    description="Response of authentication token",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="access_token", type="string"),
+     *       @OA\Property(property="token_type", type="string"),
+     *       @OA\Property(property="expires_in", type="integer"),
+     *       @OA\Property(property="user", type="string"),
+     *        )
+     *     )
+     * ),
+     */
     public function login(LoginRequest $request)
     {
         $credentials = $request->only('username', 'password');
         if (!$token = auth()->attempt($credentials)) {
-            return response()->json(['Auth error' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Unauthorized'], 401);
         }
         return $this->generateToken($token);
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/logout",
+     * summary="Logout User",
+     * security={{"bearer_token":{}}},
+     * description="Logout User",
+     * operationId="logout",
+     * tags={"Auth"},
+     * @OA\Response(
+     *    response=200,
+     *    description="User logout successfully.",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="message", type="string", example="User logout successfully.")
+     *        )
+     *     )
+     * )
+     */
     public function logout() 
     {
         auth()->logout();
         return response()->json(['message' => 'User logout successfully.']);
     }
 
+    /**
+     * @OA\Post(
+     * path="/api/token-refresh",
+     * summary="Token Refresh",
+     * security={{"bearer_token":{}}},
+     * description="Token Refresh",
+     * operationId="refresh",
+     * tags={"Auth"},
+     * @OA\Response(
+     *    response=200,
+     *    description="Refresh token data",
+     *    @OA\JsonContent(
+     *       @OA\Property(property="access_token", type="string"),
+     *       @OA\Property(property="token_type", type="string"),
+     *       @OA\Property(property="expires_in", type="integer"),
+     *       @OA\Property(property="user", type="string"),
+     *        )
+     *     )
+     * )
+     */
     public function refresh() 
     {
         return $this->generateToken(auth()->refresh());
     }
 
+    /**
+     * @OA\Get(
+     * path="/api/user",
+     * summary="Get Auth User",
+     * security={{"bearer_token":{}}},
+     * description="Get Auth User",
+     * operationId="refresh",
+     * tags={"Auth"},
+     * @OA\Response(
+     *    response=200,
+     *    description="Response of login user"
+     *     )
+     * )
+     */
     public function user() 
     {
         return response()->json(JWTAuth::user());
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,24 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+/**
+ * @OA\Info(
+ *    title="My First API",
+ *    version="1.0.0",
+ *    description="API to interact with Project",
+ *    contact={
+ *       "name": "API Support",
+ *       "url": "https://www.example.com/",
+ *       "email": "info@example.com"
+ *       }
+ * )
+ * @OA\SecurityScheme(
+ * securityScheme="bearer_token",
+ * description="Enter your API Key",
+ * type="http",
+ * scheme="bearer"
+ * )
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
